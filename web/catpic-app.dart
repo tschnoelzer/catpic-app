f2317e184f77789c7742e65bf05b4b804a9f754e
Seth Ladd <sethladd@dhcp-172-26-93-109.nyc.corp.google.com>
2012-09-20 12:30:20 -0400

    first commit

---
+++ web/catpic-app.dart
@@ -0,0 +1,13 @@
+#import('package:catpic/catpic.dart');
+#import('package:widget/widget.dart');
+#import('package:frame/frame.dart');
+
+#import('dart:html');
+
+main() {
+  var root = new Widget();
+  for (var i = 0; i < 20; i++) {
+    root.children.add(new Frame(new Catpic(i)));
+  }
+  root.runApp();
+}
